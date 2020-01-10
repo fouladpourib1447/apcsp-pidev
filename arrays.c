a932e6401df0a3e5a3cf185bd37b55eec49f680c
Bahar <fouladpourib1447@my.sduhsd.com>
2020-01-10 12:53:47 -0800

    add attempted arrays assignment

---
+++ arrays.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int arrayAdd(int s, int n)
+{
+  int arr[s];
+  int s = 100;
+  n = 1;
+
+  for (float i = 0; i<100; i++)
+{
+    arr[i] = i * i;
+}
+  printf("Array: %d\n", s, n);
+}
+
+int main()
+{
+  int a = 1;
+  printf("Array: %d\n", a);
+  int r;
+  r = arrayAdd(100, 1);
+  printf("array: s=%d, n=%d\n", s, n);
+}
+
